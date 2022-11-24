116d43a4e151ef82148539f8ca1e69379f42a1fa
Liucheng Zhu <13927181971@163.com>
2022-11-24 23:27:36 +0800

    commit 1.4

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@ using CentralControlGrpcService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Console.WriteLine(args);
-
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
@@ -14,8 +12,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<FirstTestSvc>();
 
-string handleStr = "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909";
+string handleStr = "中控通信服务";
 
 app.MapGet("/", () => handleStr);
 
